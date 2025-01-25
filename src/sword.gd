b1a584cc1d083d5d128238ae9e86ed0fcb940954
Ian Lum <ianlum314@gmail.com>
2025-01-25 17:32:38 -0500

    refactor sword.gd

--- src/sword.gd
+++ src/sword.gd
@@ -13,19 +13,25 @@ func _ready() -> void:
 	target_ball.move()
 
 func _process(_delta: float) -> void:
+	handle_rotation()
+	check_correct_rotation()
+
+func handle_rotation():
+	# Handles WASD and Shift
 	target_rotation = Vector3()
 	target_rotation.x = Input.get_axis("forward", "backward")
 	target_rotation.z = Input.get_axis("right", "left")
 	target_rotation *= 45
 	if Input.is_action_pressed("shift"):
 		target_rotation.x *= 3
-
+	rotation_degrees = rotation_degrees.lerp(target_rotation, ROTATION_LERP_SPEED)
+	
+	# Handles Spacebar
 	var yaw = 0.0
 	if Input.is_action_pressed("space"):
 		yaw = 90.0
 	sword_mesh.rotation_degrees.y = lerp(sword_mesh.rotation_degrees.y, yaw, ROTATION_LERP_SPEED)
-
-	rotation_degrees = rotation_degrees.lerp(target_rotation, ROTATION_LERP_SPEED)
 	
+func check_correct_rotation():
 	if round(rotation_degrees) == round(target_ball.rotation_degrees):
 		target_ball.move()
