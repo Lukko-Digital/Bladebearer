4ff95f93e2a4415495e294bc56077d1908887dae
Ian Lum <ianlum314@gmail.com>
2025-01-25 17:10:00 -0500

    hologram animates out to new position

--- src/target_ball.gd
+++ src/target_ball.gd
@@ -1,14 +1,22 @@
 extends Node3D
 
+const ROTATION_LERP_SPEED = 0.15
+
+var target_rot: Vector3
+
+func _process(_delta: float) -> void:
+	if round(rotation_degrees) != target_rot:
+		rotation_degrees = rotation_degrees.lerp(target_rot, ROTATION_LERP_SPEED)
+	
 func move():
-	var target_rot = Vector3(
+	var new_rot = Vector3(
 		randi_range(-1, 1),
 		0,
 		randi_range(-1, 1)
 	)
-	target_rot *= 45
+	new_rot *= 45
 	if randi() % 2:
-		target_rot.x *= 3
+		new_rot.x *= 3
 	
-	rotation_degrees = target_rot
-	print(rotation_degrees)
+	target_rot = new_rot
+	print(target_rot)
