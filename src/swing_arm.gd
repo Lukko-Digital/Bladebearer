fafcd7807c78612bcc397be334e74aa0055f9e93
Ian Lum <ianlum314@gmail.com>
2025-01-26 16:56:50 -0500

    making arm length a const, tweak swing timing

--- src/swing_arm.gd
+++ src/swing_arm.gd
@@ -1,24 +1,24 @@
 extends Node3D
 class_name SwingArm
 
+const SWING_ARM_LENGTH = 4
+
 @onready var arm: Node3D = %Arm
 
 var repositioning = false
 
+func _ready() -> void:
+	arm.position.y = SWING_ARM_LENGTH
+
 func move_to_swing_position(target_rotation: Vector3):
 	repositioning = true
 	# order matters, rotate z, then x
-	global_position = Vector3.DOWN.rotated(
+	global_position = Vector3.UP.rotated(
 		Vector3.BACK, deg_to_rad(target_rotation.z)
 	).rotated(
 		Vector3.RIGHT, deg_to_rad(target_rotation.x)
-	) * -arm.position.y
-
-	rotation_degrees = Vector3(
-		target_rotation.x - 180,
-		0,
-		-target_rotation.z
-	)
+	) * -SWING_ARM_LENGTH
+	rotation_degrees = target_rotation
 	repositioning = false
 
 func windup() -> Tween:
@@ -27,8 +27,8 @@ func windup() -> Tween:
 	windup_tween.tween_property(
 		self,
 		"rotation_degrees",
-		rotation_degrees - Vector3(0, 0, 40),
-		4
+		rotation_degrees - Vector3(0, 0, 20),
+		3
 	).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 	return windup_tween
 
@@ -46,7 +46,7 @@ func swing(hit: bool) -> Tween:
 		swing_tween.tween_property(
 			self,
 			"rotation_degrees",
-			rotation_degrees + Vector3(0, 0, 120),
+			rotation_degrees + Vector3(0, 0, 60),
 			1
 		).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 
