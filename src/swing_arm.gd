36375060274834991eb8c090c68277a442eb590a
Ian Lum <ianlum314@gmail.com>
2025-01-26 02:14:08 -0500

    swing arm swings with tween

--- src/swing_arm.gd
+++ src/swing_arm.gd
@@ -3,7 +3,13 @@ class_name SwingArm
 
 @onready var arm: Node3D = %Arm
 
+var repositioning = false
+
+func _ready() -> void:
+	swing()
+
 func move_to_swing_position(target_rotation: Vector3):
+	repositioning = true
 	# order matters, rotate z, then x
 	global_position = Vector3.DOWN.rotated(
 		Vector3.BACK, deg_to_rad(target_rotation.z)
@@ -16,3 +22,25 @@ func move_to_swing_position(target_rotation: Vector3):
 		0,
 		-target_rotation.z
 	)
+	repositioning = false
+
+func swing():
+	# windup -40 degrees
+	var windup_tween = create_tween()
+	windup_tween.tween_property(
+		self,
+		"rotation_degrees",
+		rotation_degrees - Vector3(0, 0, 40),
+		1
+	).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+
+	await windup_tween.finished
+
+	# swing +120 degrees
+	var swing_tween = create_tween()
+	swing_tween.tween_property(
+		self,
+		"rotation_degrees",
+		rotation_degrees + Vector3(0, 0, 270),
+		0.7
+	).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
