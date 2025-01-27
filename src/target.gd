9cae9afe5615aea1fee82a0cbc38bc638e4e7663
Ian Lum <ianlum314@gmail.com>
2025-01-27 09:20:08 -0500

    dont allow the same target position twice

--- src/target.gd
+++ src/target.gd
@@ -13,17 +13,24 @@ func _process(_delta: float) -> void:
 		rotation_degrees = rotation_degrees.lerp(target_rot, ROTATION_LERP_SPEED)
 	
 func move():
-	var new_rot = Vector3(
-		randi_range(-1, 1),
-		0,
-		randi_range(-1, 1)
-	)
-	new_rot *= 45
-	if randi() % 2:
-		# see sword.gd for why max is set to 181
-		new_rot.x = wrapf(180 - new_rot.x, -180, 181)
-	
-	target_rot = new_rot
+	var new_rot: Vector3
+	while true:
+		# Don't allow the same target twice, loop until new rotation is generated
+		new_rot = Vector3(
+			randi_range(-1, 1),
+			0,
+			randi_range(-1, 1)
+		)
+		new_rot *= 45
+		if randi() % 2:
+			# see sword.gd for why max is set to 181
+			new_rot.x = wrapf(180 - new_rot.x, -180, 181)
+
+		if new_rot != target_rot:
+			# Break when I see new rotation
+			target_rot = new_rot
+			break
+			
 	print(target_rot)
 
 	swing_arm.move_to_swing_position(target_rot)
