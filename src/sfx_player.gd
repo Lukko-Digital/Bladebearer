b1ee9e2082138cb817e5e6ece76b988b55c94d1a
joshuatehrlich <joshuatehrlich@gmail.com>
2025-01-29 00:32:39 -0500

    add sfx

--- src/sfx_player.gd
+++ src/sfx_player.gd
@@ -4,7 +4,12 @@ class_name SfxPlayer
 func _ready() -> void:
 	Global.sfx_player = self
 
+
+
 ## [audio_player_name] must exactly match the name of a child
 ## [AudioStreamPlayer] or [AudioStreamPlayer3D] node.
 func play(audio_player_name: String):
 	get_node(audio_player_name).play()
+
+func stop(audio_player_name: String):
+	get_node(audio_player_name).stop()
