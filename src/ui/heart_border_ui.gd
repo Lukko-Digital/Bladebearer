b5603a6b5fe0be0b4f76fc5dbb0445fe5ac11151
Ian Lum <ianlum314@gmail.com>
2025-01-29 10:36:05 -0500

    heart borders shake with camera

--- src/ui/heart_border_ui.gd
+++ src/ui/heart_border_ui.gd
@@ -1,8 +1,17 @@
 extends CanvasLayer
 class_name HeartBorderUI
 
+@export var camera: MainCamera
+
+@onready var shake_origin: Control = %ShakeOrigin
 @onready var bearer_borders: Control = %BearerBorders
 @onready var opponent_borders: Control = %OpponentBorders
+
+# Valueguess and checkeduntiluishakematched
+const SHAKE_COEF = 100
+
+func _process(_delta: float) -> void:
+	shake_origin.position = Vector2(-camera.h_offset, -camera.v_offset) * SHAKE_COEF
 
 func set_bearer_border(bearer_rank: CombatantRank):
 	set_border(bearer_rank, bearer_borders)
