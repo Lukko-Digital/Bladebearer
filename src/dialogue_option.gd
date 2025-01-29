38a080a818a6a66e016958fd3d3faaa351f6bc85
mrSquidee <mr.squidy78@gmail.com>
2025-01-28 21:25:13 -0500

    added comments for scuffed parts of code (all good)

--- src/dialogue_option.gd
+++ src/dialogue_option.gd
@@ -17,12 +17,13 @@ func set_text(text: String):
 
 func set_target_rot(rot: Vector3):
 	target_rot = rot
-	label.rotation_degrees.y = rot.z * sign(rot.x)
+	label.rotation_degrees.y = rot.z * sign(rot.x) # scuffed trial and error math to get the rotation to line up to be a little easier to see (i didn't want to do actual math)
 
 	if rot.z > 0:
 		label.rotation_degrees.z = -90
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 
+# you can't edit the animation player from imported .glb scenes, so this is a workaround to make it loop
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	if _anim_name == "Pointer_Idle":
 		animation_player.play("Pointer_Idle")
