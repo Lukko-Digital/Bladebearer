baa7c11a4e325be79717e3a14ecdb39fb27e77f6
joshuatehrlich <joshuatehrlich@gmail.com>
2025-01-29 18:13:10 -0500

    shorten light container

--- src/light_container.gd
+++ src/light_container.gd
@@ -6,8 +6,5 @@ extends Node3D
 @export var brightness: float = 1.0 :
 	set(value):
 		brightness = value
-		set_brightness(value)
-
-func set_brightness(value):
 		for light in lights:
 			light.light_energy = value
