4a10c3c192de07ee6e712560ff0f3bda6a95388c
joshuatehrlich <joshuatehrlich@gmail.com>
2025-01-28 22:36:36 -0500

    tweak heart spacing

--- src/ui/heart_holder.gd
+++ src/ui/heart_holder.gd
@@ -5,7 +5,7 @@ class_name HeartHolder
 const Z_POSITION = -0.071
 const SCALE = Vector3(0.067, 0.067, 0.067)
 # Offset assumes that scale is 0.067
-const HEART_OFFSET = 1.4
+const HEART_OFFSET = 1.6
 
 @onready var heart_scene = preload("res://src/ui/heart.tscn")
 
@@ -27,10 +27,12 @@ func set_hearts(num_hearts: int):
 	for i in range(num_hearts):
 		var heart_instance: Heart = heart_scene.instantiate()
 		heart_instance.position.x = start_x + i * HEART_OFFSET
+		heart_instance.set_rotation_degrees(Vector3(0, 0, randi_range(0, 30)))
 		add_child(heart_instance)
 		heart_array.append(heart_instance)
 
 func break_heart_at_idx(idx: int):
 	if idx < 0 or idx >= heart_array.size():
 		push_error("Invalid heart index ", str(idx), " on ", self)
+		return
 	heart_array[idx].break_heart()
