afd2644c0b7a395e42a7ece93ebf489c066652c8
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-01-29 23:07:31 -0500

    turn heart ui from control to node3d

--- src/ui/heart_border_ui.gd
+++ src/ui/heart_border_ui.gd
@@ -1,18 +1,15 @@
-extends CanvasLayer
+extends Node3D
 class_name HeartBorderUI
 
 @export var camera: MainCamera
 
-@onready var shake_origin: Control = %ShakeOrigin
-@onready var bearer_borders: Control = %BearerBorders
-@onready var opponent_borders: Control = %OpponentBorders
+@onready var shake_origin: Node3D = %ShakeOrigin
+@onready var bearer_borders: Node3D = %BearerBorders
+@onready var opponent_borders: Node3D = %OpponentBorders
 
 ## Coefficient to convert camera offset to border position such that the border
 ## stays around the hearts
 const OFFSET_COEF = 120
-
-func _process(_delta: float) -> void:
-	shake_origin.position = Vector2(-camera.h_offset, -camera.v_offset) * OFFSET_COEF
 
 func set_bearer_border(bearer_rank: CombatantRank):
 	set_border(bearer_rank, bearer_borders)
@@ -20,7 +17,7 @@ func set_bearer_border(bearer_rank: CombatantRank):
 func set_opponent_border(opponent_rank: CombatantRank):
 	set_border(opponent_rank, opponent_borders)
 
-func set_border(rank: CombatantRank, border_parent: Control):
+func set_border(rank: CombatantRank, border_parent: Node3D):
 	hide_all(border_parent)
 	# String must correspond to the name of a border node in the scene.
 	var rank_name: String
@@ -35,6 +32,6 @@ func set_border(rank: CombatantRank, border_parent: Control):
 			rank_name = "Kingsguard"
 	border_parent.get_node(rank_name).show()
 
-func hide_all(border_parent: Control):
+func hide_all(border_parent: Node3D):
 	for border in border_parent.get_children():
 		border.hide()
