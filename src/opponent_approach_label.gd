1da68273b865948b0d54f150b0801b956863f87f
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-01-31 02:36:20 -0500

    create alternate cutscene text

--- src/opponent_approach_label.gd
+++ src/opponent_approach_label.gd
@@ -2,14 +2,38 @@ extends Label3D
 class_name OpponentApproachLabel
 
 var description_map = {
-	CombatantRank.RankName.PEASANT: ["a man in ragged clothes charges forward"],
-	CombatantRank.RankName.FOOTSOLDIER: ["a uniformed soldier points his blade at you"],
-	CombatantRank.RankName.KNIGHT: ["a figure clad in armor approaches"],
-	CombatantRank.RankName.KINGSGUARD: ["a figure in regal armor blocks the way"],
+	CombatantRank.RankName.PEASANT: [
+		"a man in ragged clothes approaches with greedy eyes",
+		"a weak soul lunges from behind",
+		"you notice a gaunt face full of envy",
+		"a poor soul wishes to contest the bearer"
+	],
+	CombatantRank.RankName.FOOTSOLDIER: [
+		"a uniformed soldier points his blade",
+		"a clean shaven man readies his swing",
+		"a soldier approaches warily",
+		"you notice a soldier eyeing you from the shadows"
+	],
+	CombatantRank.RankName.KNIGHT: [
+		"a figure clad in armor approaches",
+		"sparkling armor disrupts your vision",
+		"a knight approaches, shining with metal"
+	],
+	CombatantRank.RankName.KINGSGUARD: [
+		"a figure in regal armor blocks the way",
+		"a cloaked figure approaches rapidly",
+		"you can tell he's a kingsguard by his gait. fear consumes you",
+		"a man as tall as a tree levels his blade at you"
+	],
 }
 
 var win_messages = [
-	"an easy victory"
+	"an easy victory",
+	"another unworthy wielder",
+	"the blood feels warm against your skin",
+	"must move forward",
+	"time to advance",
+	"the battlefield beckons"
 ]
 
 func _ready() -> void:
