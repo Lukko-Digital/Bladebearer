45c114c809caa1197883278573d7073c4463959d
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-01-31 07:51:03 -0500

    aditional text

--- src/opponent_approach_label.gd
+++ src/opponent_approach_label.gd
@@ -6,10 +6,17 @@ var description_map = {
 		"a man in ragged clothes approaches with greedy eyes",
 		"a weak soul lunges from behind",
 		"you notice a gaunt face full of envy",
-		"a poor soul wishes to contest the bearer"
+		"a poor soul wishes to contest the bearer",
+		"ambushed by a weakling",
+		"a poor man steps in the way",
+		"a scavenger views you as easy prey",
+		"a ragged man runs at you",
+		"a cowardly man attacks you from behind"
 	],
 	CombatantRank.RankName.FOOTSOLDIER: [
-		"a uniformed soldier points his blade",
+		"a uniformed soldier points his blade at you",
+		"a soldier attacks",
+		"a man in uniform rushes you",
 		"a clean shaven man readies his swing",
 		"a soldier approaches warily",
 		"you notice a soldier eyeing you from the shadows"
@@ -17,13 +24,15 @@ var description_map = {
 	CombatantRank.RankName.KNIGHT: [
 		"a figure clad in armor approaches",
 		"sparkling armor disrupts your vision",
-		"a knight approaches, shining with metal"
+		"a knight approaches, shining with metal",
+		"dismounting their horse, a knight challenges you"
 	],
 	CombatantRank.RankName.KINGSGUARD: [
 		"a figure in regal armor blocks the way",
 		"a cloaked figure approaches rapidly",
 		"you can tell he's a kingsguard by his gait. fear consumes you",
-		"a man as tall as a tree levels his blade at you"
+		"a man as tall as a tree levels his blade at you",
+		"a kingsguard towers over you"
 	],
 }
 
