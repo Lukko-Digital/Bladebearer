5a54cb21ccfbadc9004d2075512d7f453b657de1
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-01-30 21:39:27 -0500

    unique ui for all classes

--- src/game_sequence_handler.gd
+++ src/game_sequence_handler.gd
@@ -61,20 +61,20 @@ var opponent_health: int
 # Called when the node enters the scene tree for the first time. !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 func _ready() -> void:
 
-	locations_wheel.hide()
-	target.hide()
+	# locations_wheel.hide()
+	# target.hide()
 
-	# await dialogue_handler.tutorial()
-	# await get_tree().create_timer(1).timeout
+	# # await dialogue_handler.tutorial()
+	# # await get_tree().create_timer(1).timeout
 
-	await dialogue_handler.menu()
+	# await dialogue_handler.menu()
 
-	Global.sfx_player.transition_volume_db("PreIntroAmbience", -16, 0.5)
-	await dialogue_handler.intro()
+	# Global.sfx_player.transition_volume_db("PreIntroAmbience", -16, 0.5)
+	# await dialogue_handler.intro()
 
 	$NewBearer.modulate = Color(Color.WHITE, 0)
 	$NewBearer.hide()
-	bearer_rank = KINGSGUARD
+	bearer_rank = FOOTSOLDIER
 	init_bearer_health()
 	enter_location(6)
 	locations_wheel.set_location(6)
