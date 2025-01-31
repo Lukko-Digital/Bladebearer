7adefda0123ea6973027a5c805487993d95d2841
Ian Lum <ianlum314@gmail.com>
2025-01-30 22:57:01 -0500

    fix location wheel transparency using text modulate

--- src/ui/location_label.gd
+++ src/ui/location_label.gd
@@ -4,30 +4,27 @@ class_name LocationLabel
 
 const ROTATION_PER_STEP = 12
 
-var transparency_map = {
-	0: 0,
-	1: 0.9,
-	2: 0.95,
-	3: 0.97,
-	4: 0.98,
-	5: 0.99,
-	6: 0.99,
-}
+# Increase to increase the alpha of all labels
+const ALPHA_INTERCEPT = 0.55
+# Increase to increase the alpha falloff over distance
+const ALPHA_SLOPE = 0.7
 
-var target_transparency: float
+var target_alpha: float
 
 func update_wheel_visuals():
 	var y = position.y
 	position.z = -abs(y) / 4
 	rotation_degrees.x = -5 * y * ROTATION_PER_STEP
-	transparency = transparency_map[int(round(5 * abs(y)))]
-	target_transparency = transparency
+
+	var alpha = 1 if round(5 * abs(y)) == 0 else ALPHA_INTERCEPT - ALPHA_SLOPE * abs(y)
+	target_alpha = alpha
+	modulate = Color(Color.WHITE, alpha)
 
 func fade_in(time: float):
 	transparency = 1
 	var tween = create_tween()
-	tween.tween_property(self, "transparency", target_transparency, time)
+	tween.tween_property(self, "modulate", Color(Color.WHITE, target_alpha), time)
 
 func fade_out(time: float):
 	var tween = create_tween()
-	tween.tween_property(self, "transparency", 1, time)
+	tween.tween_property(self, "modulate", Color(Color.WHITE, 0), time)
