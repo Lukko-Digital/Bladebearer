f3c80d721455bb584d49cd5e453ba77fcbef0da7
Ian Lum <ianlum314@gmail.com>
2025-03-12 22:27:48 -0400

    scale based on z position

--- src/joystick_indicator.gd
+++ src/joystick_indicator.gd
@@ -1,12 +1,18 @@
 extends Node3D
 class_name JoystickIndicator
 
+## SET TO 0 IF YOU WANT TO FORWARD-BACKWARD SCALING
+const SCALE_COEF = -0.7
+
 @onready var single: AnimatedSprite3D = %Single
 @onready var updown: AnimatedSprite3D = %UpDown
 @onready var leftright: AnimatedSprite3D = %LeftRight
+## Expect to be an immediate child of DialogueOption
+@onready var option_origin: DialogueOption = get_parent()
 
 func _process(_delta: float) -> void:
 	global_rotation = Vector3.ZERO
+	scale = Vector3.ONE * (1 + (global_position - option_origin.global_position).z * SCALE_COEF)
 
 func play_animation(target_rot: Vector3):
 	print(target_rot)
